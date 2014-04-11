7b8b958a39a4431de8354e5700aecd9dbd85161a
Jhenna Voorhis <jhenna.voorhis@gmail.com>
2014-04-10 21:04:56 -0700

    problem set 1

---
+++ ps1/Poem.java
@@ -0,0 +1,28 @@
+//A class that displays a poem
+public class Poem {
+	//Method using Strings of text
+	public static void main(String[] args) {
+		//Creating and assigning a String variable called "repeating" for a repeating 
+		//line in the poem
+		String repeating;
+		repeating = "Tick-tock tick-tock goes the clock.";
+		//Print lines of text and blank lines to display the poem
+		System.out.println("'Tick-Tock of the Clock' by Me");
+		System.out.println("");
+		System.out.println(repeating);
+		System.out.println("");
+		System.out.println("The student sits writing at a table,");
+		System.out.println("Wishing instead to be in a fable.");
+		System.out.println("She cannot think of a thing to write,");
+		System.out.println("Of her goal she is losing sight.");
+		System.out.println("");
+		System.out.println(repeating);
+		System.out.println("");
+		System.out.println("The midnight hour does fast approach,");
+		System.out.println("Without a submission she will face reproach.");
+		System.out.println("She fianlly finishes at the late hour of one,");
+		System.out.println("Thank goodness the assignment is now done.");
+		System.out.println("");
+		System.out.println(repeating);
+	}
+}
